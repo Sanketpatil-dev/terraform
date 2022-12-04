57be9adbbf4054290c8eab387f411d7dc1ecd9e8
Sanketpatil-dev <119466096+Sanketpatil-dev@users.noreply.github.com>
2022-12-04 09:02:47 +0530

    Update deployment.tf

--- deployment.tf
+++ deployment.tf
@@ -1,3 +1,4 @@
+
 terraform {
   required_providers {
     azurerm={
@@ -6,12 +7,16 @@ terraform {
     }
   }
 }
+
+
+
 provider "azurerm" {
+        
     features {}
-  subscription_id = "4379ddc6-d045-4d91-9844-52690a3371a8"
-    client_id = "eaca2cf8-b7fb-4a1a-bb70-8ba205689ac5"  
-    client_secret ="svk8Q~4qUkS7Ba9ZpUx35.S5S4a4-vJMrGJgZb8A" 
+    
+    subscription_id = "4379ddc6-d045-4d91-9844-52690a3371a8" 
     tenant_id = "0df31d06-8b24-4218-90a0-e08bb052bef6"
+
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -21,7 +26,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_service_plan" "service_plan" {
-  name="Devops_Service_Plan1"
+  name="Devops_Service_Plan"
   location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type = "Linux"
@@ -30,7 +35,7 @@ resource "azurerm_service_plan" "service_plan" {
 }
 
 resource "azurerm_linux_web_app" "Webapp" {
-    name = "Devops-prod-pathways1"
+    name = "Devops-prod-pathways"
     location = azurerm_resource_group.rg.location
     service_plan_id = azurerm_service_plan.service_plan.id
     resource_group_name = azurerm_resource_group.rg.name
@@ -46,11 +51,10 @@ resource "azurerm_linux_web_app" "Webapp" {
     }
 
 
-
-
-
 resource "azurerm_app_service_source_control" "source_control" {
   app_id = azurerm_linux_web_app.Webapp.id
   repo_url = "https://github.com/Sanketpatil-dev/group-proj"
   branch = "master" 
+  
+  
 }
